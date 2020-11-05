65a44f67d0c8c82d69b7d8553ba7be0dce77e4ea
Lasitha <lasitha@gatech.edu>
2020-11-05 12:30:11 -0500

    modified bash file

--- cmake_build.sh
+++ cmake_build.sh
@@ -7,23 +7,28 @@ git submodule update
 echo "Installing CNPY for storing data!"
 mkdir ./third-party/cnpy/build
 cd ./third-party/cnpy/build
-cmake ..
+cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j8
 sudo make install
+
+cd ../../..
 
 echo "Installing ModernRootics for IK!"
-mkdir ./third-party/ModerRobotics/build
+mkdir ./third-party/ModernRobotics/build
 cd ./third-party/ModernRobotics/build
-cmake ..
-make -j8
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j8 
 sudo make install
 
+cd ../../..
 echo "Installing admm standalone"
 mkdir ./standalone/build
 cd ./standalone/build
-cmake ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCNPY_DIR="../third-party/cnpy/cmake" -DModernRoboticsCpp_DIR="../third-party/ModernRobotics/cmake" ..
 make -j8
 sudo make install
+
+cd ../../
 
 echo "Installation Complete"
 
